7d83f77381f8a94d89501b13a1f1424c39bf2f5f
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-05-09 20:00:03 -0300

    feat: create a function to get information of a endpoint

--- pkg/requests/requests.go
+++ pkg/requests/requests.go
@@ -9,7 +9,6 @@ import (
 )
 
 func SendRequest(url string, data interface{}, httpMethod string, bearer interface{}) error {
-
 	jsonBody, err := json.Marshal(data)
 	if err != nil {
 		return fmt.Errorf("failed to marshal account data: %v", err)
@@ -46,4 +45,29 @@ func SendRequest(url string, data interface{}, httpMethod string, bearer interfa
 	}
 
 	return nil
+}
+
+func SendRequestGet(url, bearerToken string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error to create request: %v", err)
+	}
+
+	req.Header.Add("Authorization", "Bearer "+bearerToken)
+	req.Header.Add("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error to send request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error to read the body of response: %v", err)
+	}
+
+	return body, nil
 }
