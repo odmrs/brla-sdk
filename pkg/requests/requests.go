462db9b678259a42b554a908992f3567ec48f1f6
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-06-10 18:12:28 -0300

    feat: add to requests accept query params and return just the necessary things

--- pkg/requests/requests.go
+++ pkg/requests/requests.go
@@ -6,23 +6,29 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 )
 
-func SendRequest(url string, data interface{}, httpMethod string, bearer interface{}) error {
+func SendRequest(
+	url string,
+	data interface{},
+	httpMethod string,
+	bearer interface{},
+) (string, error) {
 	jsonBody, err := json.Marshal(data)
 	if err != nil {
-		return fmt.Errorf("failed to marshal account data: %v", err)
+		return "", fmt.Errorf("failed to marshal account data: %v", err)
 	}
 
 	bodyRender := bytes.NewReader([]byte(jsonBody))
 
 	req, err := http.NewRequest(httpMethod, url, bodyRender)
 	if err != nil {
-		return fmt.Errorf("HTTP request failed: %v", err)
+		return "", fmt.Errorf("HTTP request failed: %v", err)
 	}
 
 	if bearer != nil {
-		var tokenBearer string = "Bearer " + bearer.(string)
+		tokenBearer := "Bearer " + bearer.(string)
 		req.Header.Add("authorization", tokenBearer)
 	}
 	req.Header.Add("accept", "application/json")
@@ -31,24 +37,51 @@ func SendRequest(url string, data interface{}, httpMethod string, bearer interfa
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("HTTP request failed %v", err)
+		return "", fmt.Errorf("HTTP request failed %v", err)
 	}
 
 	defer req.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("failed to read response body: %v", err)
-	}
-	if len(body) != 0 {
-		return fmt.Errorf("\n\t\t\tAPI error: %s", string(body))
+		return "", fmt.Errorf("failed to read response body: %v", err)
 	}
 
-	return nil
+	if len(body) != 0 {
+		var responseMap map[string]interface{}
+		if err := json.Unmarshal(body, &responseMap); err == nil {
+			if accessToken, ok := responseMap["accessToken"].(string); ok {
+				return accessToken, nil
+			}
+			if pix, ok := responseMap["brCode"].(string); ok {
+				return pix, nil
+			}
+		}
+		return "", fmt.Errorf("\n\t\t\tAPI error: %s", string(body))
+	}
+
+	return "", nil
 }
 
-func SendRequestGet(url, bearerToken string) ([]byte, error) {
-	req, err := http.NewRequest("GET", url, nil)
+func SendRequestGet(
+	urlStr string,
+	queryParams map[string]string,
+	bearerToken string,
+) ([]byte, error) {
+	baseUrl, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, fmt.Errorf("error to parse URL: %v", err)
+	}
+
+	if queryParams != nil && len(queryParams) > 0 {
+		params := url.Values{}
+		for key, value := range queryParams {
+			params.Add(key, value)
+		}
+
+		baseUrl.RawQuery = params.Encode()
+	}
+	req, err := http.NewRequest("GET", baseUrl.String(), nil)
 	if err != nil {
 		return nil, fmt.Errorf("error to create request: %v", err)
 	}
